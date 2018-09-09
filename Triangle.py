8457ee9fedce20a9f182c8cf7ce99884bbc59200
engeljb10 <engeljb10@gmail.com>
2018-09-09 16:32:43 -0400

    fixed logic

--- Triangle.py
+++ Triangle.py
@@ -29,32 +29,29 @@ def classifyTriangle(a,b,c):
 
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
-        print("here1")
         return 'InvalidInput'
         
     if a <= 0 or b <= 0 or c <= 0:
-        print("here2")
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        print("here3")
         return 'InvalidInput';
       
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
+    if a == b and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif ((a ** 2) + (b ** 2) == (c ** 2)) or ((a ** 2) + (c ** 2) == (b ** 2)) or ((c ** 2) + (b ** 2) == (a ** 2)) :
         return 'Right'
     elif (a != b) and  (b != c) and (a != b):
         return 'Scalene'
     else:
-        return 'Isoceles'
+        return 'Isosceles'
