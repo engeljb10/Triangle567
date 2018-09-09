c2e232b7f103ab3be07cc22297c8d09997f554fe
engeljb10 <engeljb10@gmail.com>
2018-09-09 16:20:11 -0400

    adding correct test file

--- test_Triangle.py
+++ test_Triangle.py
@@ -8,6 +8,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 """
 
 import unittest
+import pytest
 
 from Triangle import classifyTriangle
 
@@ -25,6 +26,26 @@ class TestTriangles(unittest.TestCase):
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testIsoscelesTriangles(self): 
+        self.assertEqual(classifyTriangle(3,3,4),'Isosceles','3,3,4 should be isosceles')
+
+    def testScaleneTriangles(self): 
+        self.assertEqual(classifyTriangle(2,3,4),'Scalene','2,3,4 should be scalene')
+
+    def testNotATriangle(self): 
+        self.assertEqual(classifyTriangle(2,3,9),'NotATriangle','2,3,9 is not a triangle')
+
+    def testInvalidInputInt(self): 
+   	    self.assertEqual(classifyTriangle(2.5,3,9),'InvalidInput','2.5 is not an integer')
+
+    def testInvalidInputZero(self): 
+        self.assertEqual(classifyTriangle(0,3,5),'InvalidInput','0 is invalid')
+
+    def testInvalidInput200(self): 
+        self.assertEqual(classifyTriangle(201,3,2),'InvalidInput','201 exceeds maximum input')
+
+
 
 if __name__ == '__main__':
     print('Running unit tests')
